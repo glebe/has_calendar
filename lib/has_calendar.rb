d1e351907612a11587bb0ffe0622a7860a37a712
Maximilian Schulz <max@jungeelite.de>
2009-06-09 17:15:11 +0200

    Heavy refactoring in order to simplify the readability. Removed "cal" dependency and replaced it with ruby code.

--- lib/has_calendar.rb
+++ lib/has_calendar.rb
@@ -2,7 +2,7 @@ module SimplesIdeias
   module Calendar
     module ActionView
       def calendar(options={}, &block)
-        options = {
+        @options = options = {
           :year => Date.today.year,
           :month => Date.today.month,
           :today => nil,
@@ -12,92 +12,73 @@ module SimplesIdeias
           :caption_format => :default,
           :id => "calendar"
         }.merge(options)
-      
-        cmd = 'cal '
-        cmd << '-m ' unless RUBY_PLATFORM =~ /darwin/
-      
-        # execute the command
-        output = IO.popen("#{cmd} #{options[:month]} #{options[:year]}").read
-      
-        # get calendar lines
-        io = StringIO.new(output)
-        lines = io.readlines
-        lines = lines[2, lines.size-3]
-        lines.map!(&:chomp)
-      
-        # strip spaces from each day and group them
-        days = lines.inject([]) do |holder, line|
-          0.step(line.size, 3) do |n|
-            holder << line[n, 3].squish
-          end
-          holder
-        end
         
-        # group all records if data is provided
-        if options[:events]
-          records = options[:events].inject({}) do |memo, record|
-            stamp = record.send(options[:field]).to_date.to_s(:number)
-            memo[stamp] ||= []
-            memo[stamp] << record
-            memo
-          end
-        end
+        # If provided, group events by the day of their occuring
+        @records = group_events(options[:events], options[:field])
+        
+        # Get the list of all days which we will present in the calendar
+        days = days_for_calendar
       
-        # building the calendar
-        contents = content_tag(:table, :id => options[:id], :class => 'calendar') do
-          # first, get the header
-          today = Date.today
-          date = today.beginning_of_week
-          date = date - 1.day if RUBY_PLATFORM =~ /darwin/
-        
-          caption = content_tag(:caption, l(Date.new(options[:year], options[:month], today.day), :format => options[:caption_format]))
-        
-          head = content_tag(:thead) do
-            content_tag(:tr) do
-              (0..6).collect { |i| content_tag(:th, l(date + i.days, :format => options[:header_format])) } * ""
-            end
-          end
-        
-          # then get the body
+        # Building the calendar
+        contents = content_tag(:table, :id => options[:id], :class => 'monthly calendar') do
           rows = ""
-          days.in_groups_of(7, "") do |group|
-            rows << content_tag(:tr) do
-              group.inject("") do |cols, day|
-                col_options = {:class => 'day'}
-                events = ""
-              
-                unless day.blank?
-                  date = Date.new(options[:year], options[:month], day.to_i)
-                  col_options[:class] << ' today' if today == date
-                  col_options[:class] << ' weekend' if [0,6].include?(date.wday)
-                end
-              
-                if block_given? && !day.blank?
-                  if options[:events]
-                    events = capture(date, records[date.to_s(:number)], &block)
-                  else
-                    events = capture(date, &block)
-                  end
-                  
-                  col_options[:class] << ' events' unless events.blank?
-                end
-              
-                cols << content_tag(:td, col_options) do
-                  day = options[:today] if options[:today] && date == today
-                  span = content_tag(:span, day)
-                  span + events
-                end
-              end
-            end
+          days.in_groups_of(7, "") do |week_days|
+            rows << content_tag(:tr, week_days.inject("") { |cols, day| cols << html_for_day(day, &block) })
           end
-        
-          caption + head + rows
+          table_caption + table_header + rows
         end
-      
-        concat(contents) if block_given?        
         
+        # If a block is given, replace it by the calendar
+        concat(contents) if block_given?
+        
+        # Or maybe the user just wanna render a simple calendar
         contents
       end
+      
+      def table_caption
+        content_tag(:caption, I18n.l(Date.civil(@options[:year], @options[:month]), :format => @options[:caption_format]))
+      end
+      
+      # Create the calendar table header consisting of seven th fields in a row
+      def table_header
+        first_day_of_week = Date.today.beginning_of_week
+        content_tag(:thead) do
+          content_tag(:tr) do
+            (0..6).collect { |i| content_tag(:th, l(first_day_of_week + i, :format => @options[:header_format])) }.to_s
+          end
+        end
+      end
+      
+      def days_for_calendar
+        base_day = Date.civil(@options[:year], @options[:month], 1)
+        (base_day.beginning_of_week..base_day.end_of_month.end_of_week).to_a
+      end
+      
+      
+      def html_for_day(day, &block)
+        col_options = { :class => 'day' }
+        events_html = ""
+        
+        if block_given? && !day.blank?
+          events_html = @records ? capture(day, @records[day.mjd], &block) : capture(day, &block)
+        end
+        
+        unless day.blank?
+          col_options[:class] << ' today' if Date.today == day
+          col_options[:class] << ' weekend' if [0,6].include?(day.wday)
+          col_options[:class] << ' marked' if @options[:marked] = day
+          col_options[:class] << ' events' unless events_html.blank?
+        end
+        
+        content_tag(:td, col_options) do
+          content_tag(:div, I18n.l(day, :format => "%d"), :class => 'day_indicator') + events_html
+        end
+      end
+      
+      def group_events(events, field)
+        events.group_by { |event| event.send(field).to_date.mjd } if events
+      end
+      
     end
   end
 end
